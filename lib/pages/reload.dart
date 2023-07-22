a08d310d3da024e6ffc7649940a76e97f292c843
kobbekaduwa22 <nimsara22cha@gmail.com>
2023-07-22 15:19:01 +0530

    create page

---
+++ lib/pages/reload.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Reload extends StatefulWidget {
+  const Reload({super.key});
+
+  @override
+  State<Reload> createState() => _ReloadState();
+}
+
+class _ReloadState extends State<Reload> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
