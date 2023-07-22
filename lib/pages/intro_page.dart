a67defe485ce01c9e489b01f0f0da36a53d3916e
HMAHD <hmahdharmasiri@students.nsbm.ac.lk>
2023-07-22 15:41:34 +0530

    widget

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,54 @@
+class introPage extends StatelessWidget {
+  const introPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: Column(children: [
+      Expanded(
+        flex: 2,
+        child: SizedBox(
+          width: double.infinity,
+          child: Center(
+            child: Image.asset(
+              "assets/unifit_logo.png",
+              width: size.width / 1,
+            ),
+          ),
+        ),
+      ),
+      Expanded(
+        flex: 1,
+        child: Container(
+          width: double.infinity,
+          color: Colors.white,
+          alignment: Alignment.bottomCenter,
+        ),
+      ),
+    ]));
+  }
+}
+
+class Square extends StatelessWidget {
+  final Color color;
+
+  const Square(
+    this.color, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Padding(
+      padding: const EdgeInsets.only(left: 8, right: 8),
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Container(
+          color: color,
+        ),
+      ),
+    ));
+  }
+}
