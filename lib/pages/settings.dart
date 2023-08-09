b375c3861597b8dcc09b3d0c9b28dbac6f060ac2
HMAHD <hmahdharmasiri@students.nsbm.ac.lk>
2023-08-10 00:31:46 +0530

    Settings

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 
+@override
 AppBar buildAppBar(BuildContext context, String title) {
   return AppBar(
     backgroundColor: const Color.fromARGB(220, 28, 232, 164),
@@ -74,8 +74,90 @@ class Settings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context, 'Settings'), // Pass the title here
-      body: Center(
-        child: Text('Settings Page Content'),
+      body: Container(
+        color: Color(0xFFE4F7F2),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SettingsItem(
+              icon: Icons.person,
+              title: 'Edit Profile',
+              onTap: () {
+                // Navigate to Edit Profile page
+              },
+            ),
+            SizedBox(height: 20),
+            SettingsItem(
+              icon: Icons.security,
+              title: 'Security',
+              onTap: () {
+                // Navigate to Security page
+              },
+            ),
+            SizedBox(height: 20),
+            SettingsItem(
+              icon: Icons.notifications,
+              title: 'Notification',
+              onTap: () {
+                // Navigate to Notification page
+              },
+            ),
+            SizedBox(height: 20),
+            SettingsItem(
+              icon: Icons.payment,
+              title: 'Payment',
+              onTap: () {
+                // Navigate to Payment page
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsItem extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final VoidCallback onTap;
+
+  const SettingsItem({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: Colors.black, size: 30),
+            SizedBox(width: 20),
+            Text(
+              title,
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
