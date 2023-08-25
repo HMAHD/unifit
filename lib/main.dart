85f897f3f86ec00ad4f95465413960b90d630b09
Amasha1999 <96760573+Amasha1999@users.noreply.github.com>
2023-08-25 21:33:47 +0530

    scanner template

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 //import 'package:unifit/pages/dashboard.dart';
-import 'package:unifit/pages/intro_page.dart';
+//import 'package:unifit/pages/intro_page.dart';
+import 'package:unifit/pages/scanner.dart'; 
+
+
 
 void main() => runApp(const MyApp());
 
@@ -10,8 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      
       debugShowCheckedModeBanner: false,
-      home: introPage(),
+      title: "QR Scanner",
+      home: QRScanner() 
+    
+
     );
   }
 }
