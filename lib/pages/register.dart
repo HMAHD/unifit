7015b0f5dfe46a49202f39ab153083dfa82d6090
AA Wijewardhana <aawijewardhana@students.nsbm.ac.lk>
2023-08-30 16:19:34 +0530

    Register page
    register page interface is not like another page.

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,149 +8,182 @@ class HomePage extends StatefulWidget {
 }
 
 class _EditProfileState extends State<HomePage> {
-  final logger = Logger();
-
-  PreferredSizeWidget getCustomAppBar(BuildContext context) {
-    return PreferredSize(
-      preferredSize: const Size.fromHeight(
-          140.0), // Set the preferred height for the AppBar
-      child: AppBar(
-        title: const Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            "UniFit",
-            style: TextStyle(
-              color: Color(0xFF0C303D),
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        leading: null, // Remove the SizedBox to keep the AppBar title centered
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: Image.asset(
-                'assets/app_logo.png', // Replace with your actual image path
-                width: 40,
-                height: 40,
-              ),
-            ),
-          ),
-        ],
-
-        titleSpacing: 0.0,
-        centerTitle: true,
-        toolbarHeight: 100.2,
-        toolbarOpacity: 0.8,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(25),
-            topRight: Radius.circular(25),
-            bottomRight: Radius.circular(25),
-            bottomLeft: Radius.circular(25),
-          ),
-        ),
-        elevation: 0.00,
-        backgroundColor: const Color.fromRGBO(70, 245, 202, 1),
-      ),
-    );
-  }
-
+  //bool _isObscure = true;  // show the password or not
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          const SizedBox(height: 35), // Empty space before the AppBar
-          getCustomAppBar(context), // Custom AppBar
-          const SizedBox(height: 20), // Empty space after AppBar
-
-          // Add other content of the page below the AppBar
-          const Text(
-            'Create an account', // Replace this with the desired text
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-          const Text(
-            'Unlock your fitness potential  on UNIFIT!', // Replace this with the desired text
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.black,
-            ),
-          ),
-
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter Your Username',
+          const SizedBox(height: 10),
+          getCustomAppBar(context),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  const SizedBox(height: 30),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      'Create an account',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      "Unlock your fitness potential on UNIFIT !",
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter Your Username',
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter Your Email',
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter Your Phone Number',
+                      ),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: 250,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "Sign Up",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text(
+                              'Already have an account? ',
+                              style: TextStyle(fontSize: 15),
+                            ),
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter Your Email',
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter Your Phone Number',
-              ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter Your Password',
-              ),
-            ),
-          ),
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: ElevatedButton(
-              onPressed: () {
-                // Add your sign-in logic here
-                // For example, you can call a sign-in function or navigate to another screen upon successful sign-in.
-                // Replace the below line with your actual sign-in implementation.
-                logger.i('Sign-in button pressed');
-              },
-              style: ElevatedButton.styleFrom(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 100, vertical: 25),
-                // Customize other button styles here, if needed.
-              ),
-              child: const Text('Sign In'),
-            ),
-          ),
-
-          const Text(
-            'Unlock your fitness potential  on UNIFIT!', // Replace this with the desired text
-            style: TextStyle(
-              fontSize: 15,
-              color: Colors.black,
-            ),
-          ),
-
-          // Left-aligned text using Row widget
         ],
       ),
     );
   }
+
+  // Add other content of the page below the TextField
+}
+
+PreferredSizeWidget getCustomAppBar(BuildContext context) {
+  return PreferredSize(
+    preferredSize:
+        const Size.fromHeight(140.0), // Set the preferred height for the AppBar
+    child: AppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.menu),
+        tooltip: 'menu Icon',
+        onPressed: () {},
+      ),
+      title: const Text(
+        "UniFit",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 30.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(
+            Icons.account_box,
+          ),
+          tooltip: 'Account Icon',
+          onPressed: () {},
+        ),
+      ],
+      titleSpacing: 00.0,
+      centerTitle: true,
+      toolbarHeight: 100.2,
+      toolbarOpacity: 0.8,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+          bottomRight: Radius.circular(25),
+          bottomLeft: Radius.circular(25),
+        ),
+      ),
+      elevation: 0.00,
+      backgroundColor: const Color.fromRGBO(70, 245, 202, 1),
+    ),
+  );
 }
